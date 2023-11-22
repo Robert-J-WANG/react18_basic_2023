cb4f6b5bcbd6a4f31bb98b3e18466acf3b833494
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-22 19:06:16 +1300

    3-07-router-配置404页面路由

---
+++ 3-07-router-配置404页面路由/pages/Layout/Board/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Board = () => {
+  return <div>我是二级路由页面Board</div>;
+};
---
+++ 3-07-router-配置404页面路由/pages/Layout/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+export const Layout = () => {
+  return (
+    <div>
+      <h1>我是一级路由页面Layout</h1>
+      <div>
+        {/* <Link to="/about">about</Link> */}
+        {/*  设置默认的二级路由 */}
+        <Link to="/">about</Link>
+      </div>
+      <div>
+        <Link to="/board">board</Link>
+      </div>
+
+      {/* 二级路由渲染位置 */}
+      <Outlet />
+    </div>
+  );
+};
